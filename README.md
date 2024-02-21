bbfb86cb04d6b57da9941fc230bb5d811a40e71e
Eric K <54124297+Nyveon@users.noreply.github.com>
2024-02-21 13:43:42 -0300

    feat: preliminary index + mascot

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # FCFM Ramos
 
 Opiniones!!
+
+Scoring:
+
+- Verify email: 5p (base)
+- Like/dislike a course: 2p
+- Like/dislike a text review: 1p
+- Rate carga: 5p
+  - First to rate this course: +5p
+- Rate utilidad: 5p
+  - First to rate this course: +5p
+- Rate dificultad: 5p
+  - First to rate this course: +5p
+- Write a summary: 15p
+  - First to summarize this course: +10p
+- Write a review: 15p
+  - First to review this course: +10p
+- Summary receives a like: 1p
+- Review receives a like: 1p
+
+
+Dificultad:
+
+Dificultad:
+1. 🎁 Regalado
+2. 😎 Manejable
+3. 😬 Desafiante
+4. 🤯 Duro
+5. 💀 Intenso
+
+Carga:
+1. 😴 Muy ligera
+2. 🥱 Ligera
+3. 😅 Moderada
+4. 😰 Pesada
+5. 🥵 Muy pesada
+
+Utilidad:
+1. 🙃 Inútil
+2. 🙂 Básica
+3. 😊 Valiosa
+4. 😃 Esencial
+5. 🤩 Vital
+
+
+
+Dificultad:
+1. 🥱 Trivial
+2. 😎 Manejable
+3. 😬 Desafiante
+4. 😫 Duro
+5. 💀 Letal
+
+Carga:
+1. 🎁 Regalado
+2. 😴 Ligero
+3. 😅 Moderado
+4. 😰 Pesado
+5. 🥵 Brutal
+
+Utilidad:
+1. 🙃 Inútil
+2. 🙂 Básico
+3. 🤓 Valioso
+4. 🧠 Esencial
+5. 😭 Vital
+
+Like/dislike ratio:
+1. 💖 Favorito
+2. ❤ Amado
+3. 🤔 Controversial
+4. 😡 Odiado
+5. 😈 Demonio
--- fcfmramos/view/forms/accounts.py
+++ fcfmramos/view/forms/accounts.py
@@ -3,6 +3,10 @@ from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, Length
 
 
+INVALID_EMAIL = "Correo inválido"
+PLACEHOLDER_EMAIL = {"placeholder": "nombre.apellido@ug.uchile.cl"}
+
+
 def validate_email_domain(form, field):
     if not field.data.endswith("@ug.uchile.cl"):
         raise ValidationError("El correo debe terminar en @ug.uchile.cl")
@@ -11,8 +15,8 @@ def validate_email_domain(form, field):
 class LoginForm(FlaskForm):
     email = StringField(
         "Correo",
-        render_kw={"placeholder": "nombre.apellido@ug.uchile.cl"},
-        validators=[DataRequired(), Email()]
+        render_kw=PLACEHOLDER_EMAIL,
+        validators=[DataRequired(), Email(INVALID_EMAIL)],
     )
     password = PasswordField(
         "Contraseña", validators=[DataRequired(), Length(min=8)]
@@ -23,8 +27,12 @@ class LoginForm(FlaskForm):
 class SignupForm(FlaskForm):
     email = StringField(
         "Correo",
-        render_kw={"placeholder": "nombre.apellido@ug.uchile.cl"},
-        validators=[DataRequired(), Email(), validate_email_domain],
+        render_kw=PLACEHOLDER_EMAIL,
+        validators=[
+            DataRequired(),
+            Email(INVALID_EMAIL),
+            validate_email_domain,
+        ],
     )
     password = PasswordField(
         "Contraseña", validators=[DataRequired(), Length(min=8)]
@@ -35,7 +43,11 @@ class SignupForm(FlaskForm):
 class RecoverPasswordForm(FlaskForm):
     email = StringField(
         "Correo (@ug.uchile.cl)",
-        render_kw={"placeholder": "nombre.apellido@ug.uchile.cl"},
-        validators=[DataRequired(), Email(), validate_email_domain],
+        render_kw=PLACEHOLDER_EMAIL,
+        validators=[
+            DataRequired(),
+            Email(INVALID_EMAIL),
+            validate_email_domain,
+        ],
     )
     submit = SubmitField("Recuperar contraseña")
